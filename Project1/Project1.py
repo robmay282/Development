d5171b9e4dfc02a14281e541bef6ec951739c376
robmay282 <robinmaynard76@outlook.com>
2023-11-30 19:51:14 -0500

    username and password testing completed

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -1,17 +1,11 @@
 import re 
 
-# Do I need any modules? If so, as a best practice, import them at the top of the file
-
 '''
 Project Description: Website Sign-Up
 In this project, you will write a program that prompts a user to sign up for a website. The user must come up with a username and password, then log in using the username and password.
 There are requirements for both the username and password. If either one doesn’t meet the requirements, print an appropriate error message and ask the username for a new username and password.
 If they are both valid, store them as variables. Then ask the user to log in using the username and password they chose.
 '''
-
-# Problem: How should I start? 
-# Solution: Your program should start with a print statement clearly explaining to the user both the username and password requirements. 
-# Test, push code, test push code, test push code :)
 
 # print("Please follow these requirements when creating your account. \n"
 #                      "USERNAME REQUIREMENTS: \n"
@@ -27,7 +21,7 @@ If they are both valid, store them as variables. Then ask the user to log in usi
 #                      "-Doesn't contain any spaces")
 
 
-# Define error messages
+# Define messages as variables
 
 # Set invalid username error message as a variable
 invalid_username = "Invalid username"
@@ -53,57 +47,51 @@ incorrect_login = "Incorrect username or password"
 
 while True:
 
-    username = input('Please enter a username: ')   # get username
-    password = input('Please enter a password: ')   # get password
-    taken_names = ['admin', 'admin123', 'root']     # list oftaken name list
-    sys_username = input('Enter your username: ')
-    sys_password = input('Enter your password: ')
+    new_username = input('Please enter a username: ')    # get username
+    new_password = input('Please enter a password: ')    # get password
+    taken_names = ['admin', 'admin123', 'root']          # list oftaken name list
 
-    if username in taken_names:                     # username test if already taken
+
+    if new_username in taken_names:                 # username test if already taken
         print(duplicate_username)                   # user gets error message variable that will print username already taken.
         continue                                    # loops user back to input - please enter a username
 
-    if username[0].islower() == False:              # test for first character lower case
+    if new_username[0].islower() == False:          # test for first character lower case
        print(invalid_username)                      # user gets error mesaage variable invalid username because first letter of username is not a lowercase letter.
        continue                                     # loops user back to iput - please enter a username  
     
-    test_username_match = re.fullmatch('[a-z0-9A-Z_]+',username)     # test if username only contains letters, numbers or underscore
+    test_username_match = re.fullmatch('[a-z0-9A-Z_]+',new_username)     # test if username only contains letters, numbers or underscore
     if test_username_match == None:
         print(invalid_username)
-    
-    if len(password) <= 8:                           # password test - At least 8 characters long
-        print(invalid_password)
-
-
-    
-
-
-    # print(type(test_username_match))
-    # if test_username_match:
-    #     print(" I AM FALSE")                                      # username contains a character other than letters, numbers or underscore and prints error variable invalid username
-    #     continue
-
-    # print("i am here")    
-    # if len(password) < 8:
-    #     print(invalid_password)
-    # print("I am here")                                                     # loops user back to input - please enter a username
-    # password = input('Please enter a password: ')   # get password 
-    # if len(password) <= 8:                        # test username length is greater than or equal to 8 
-    #    print(invalid_password)
         
 
-    # Solution(s): At least 8 characters long
-#   Contains at least one uppercase letter
-#   Contains at least one lowercase letter
-#   Contains at least one digit
-#   Contains at least one of these characters: !, ?, @, #, $, ^, &, *, _, -
+    SpecialChara = ['!', '?', '@', '#', '$', '^', '&', '*', '_', '-']    # Begin password testing with function
+        
+    if len(new_password) <= 8:                                       # test for at least 8 charaters
+        print(invalid_password)
+          
+    if not any(char.isdigit() for char in new_password):             # test for at least one digit
+       print(invalid_password)
+
+    if not any(char.isupper() for char in new_password):             # test for at least one uppercase letter
+       print(invalid_password)
+
+    if not any(char.islower() for char in new_password):             # test for at least one lowercase letter
+       print(invalid_password)
+
+    if not any(char in SpecialChara for char in new_password):       # test for at least one special charater
+       print(invalid_password)
+
+    else:
+        print(successful_signup)
+
+    
+  
+       
+    
+
 #   Doesn’t contain any spaces
     
-# Problem: How are we managing error messages?
-# Solution(s): We can serve error messages from strings in variables. We can serve error messages from a collection instrument such as a list or a tuple. Bad practice would be hardcoding error messages in our code. Though it works, it would be come difficult to handle and manage should a program be scaled up in the future or new error messages are added in the future with features. Handle this before you begin your while loop
-# Test, push code, test push code, test push code :)
-
-
 
 # Problem: How are we testing the username requirements?
 # Solution(s): It must start with a lowercase letter and only contain letters, numbers, and underscores.
