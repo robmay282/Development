a72441cc919e029966df727c501d11ba172257ae
robmay282 <robinmaynard76@outlook.com>
2024-01-03 19:06:30 -0500

    working withdraw tested

--- class21/module.py
+++ class21/module.py
@@ -9,7 +9,7 @@ class BankAccount:
         self.owner = owner
         self.balance = balance
         self.transactions = []
-        pass
+       
 
     # # _str_
     def __str__(self):
@@ -23,36 +23,44 @@ class BankAccount:
         #print(self.transactions)
 
     # # withdraw_funds()
-    # def __withdraw_funds__(self):
-    #     pass
+    def withdraw_funds(self, amount):
+        t = {'owner': self.owner, 'type': 'withdrawal', 'amount': amount}
+        self.transactions.append(t)
+        print(self.transactions)
+       
 
     # # get_balance()
-    # def __get_balance__(self):
+    # def get_balance(self):
     #     pass
 
     # # get_transctions()
-    # def __get_transctions__(self):
+    # def get_transctions(self):
     #     pass
 
     # # transaction_count()
-    # def __transaction_count__(self):
+    # def transaction_count(self):
     #     pass
 
     # # transaction_history()
-    # def __transaction_history__(self):
+    # def transaction_history(self):
     #     pass
 
     # # save_transaction()
-    # def __save_transaction__(self):
+    # def save_transaction(self):
     #     pass
 
 # create my class instance
 jeans_account = BankAccount('jean', 4000)
 majestic_account = BankAccount('majestic', 5000)
 # testing __str__
-print(jeans_account)
+# print(jeans_account)
 
 #testing deposit
 jeans_account.deposit_funds(50)
 jeans_account.deposit_funds(200)
 majestic_account.deposit_funds(200)
+
+#testing withdraw
+jeans_account.withdraw_funds(25)
+jeans_account.withdraw_funds(100)
+jeans_account.withdraw_funds(50)
